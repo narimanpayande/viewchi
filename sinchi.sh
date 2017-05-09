14cd797b8d2b603a435aa2a5d8e34f7b791aba62
narimanpayande <memari1350@gmail.com>
2017-05-09 05:32:28 +0430

    Update sinchi.sh

---
+++ sinchi.sh
@@ -0,0 +1,7 @@
+-#!/bin/bash 		 +python viewchi.py
+ - COUNTER=0		
+ -while [  $COUNTER -lt 5 ]; do		
+ -kill $(pgrep telegram-cli)		
+ -   ./tg -s ./bot.lua		
+ -sleep 1 # Relaunch Time		
+ -done		
