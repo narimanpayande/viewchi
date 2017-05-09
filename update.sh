f0563195ba3cc54718292bf259d9c5405d4d7af7
narimanpayande <memari1350@gmail.com>
2017-05-09 05:49:51 +0430

    Update update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+killall screen		  killall screen
+  killall tmux		  killall tmux
+ +killall tg
+  lua update.lua		  lua update.lua
+  chmod +x viewchi-0.sh		  chmod +x viewchi-0.sh
+ +clear
+  screen ./on.sh		  screen ./on.sh
