38f336eb22f391b1c42d5a75c3f72a0bd901c769
narimanpayande <memari1350@gmail.com>
2017-05-09 05:05:07 +0430

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+ aa() {
+   sudo apt-get install
+  }
+ -bb() {
+ - sudo luarocks install
+ -}
+  function logo1_SinChi() {
+  green "     >>>>               Welcome to SinChi bot                         <<<<      "
+  white "     >>>>             I wanna install it for you                      <<<<      "
